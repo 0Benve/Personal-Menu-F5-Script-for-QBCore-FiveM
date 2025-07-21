ecc17bd0c7f20b4839deaf6d19618ce80ceee6ea
0Benve <benvescript@gmail.com>
2025-07-21 19:06:47 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+
+author '0Benve'
+description 'Personal Menu F5 Script for QBCore FiveM, benve https://github.com/0Benve'
+version '1.0.0'
+
+shared_script {
+  '@ox_lib/init.lua'
+}
+
+client_script {
+  'menu/client.lua'
+}
